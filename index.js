1de00cabf390c90b719ed453482a50686d614daf
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-08-10 23:21:36 +0100

    set up web application and server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
